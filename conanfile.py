650a2ef810390302f32a9e45370301419a4eca6c
whs31 <riazantsev.dl@edu.spbstu.ru>
2025-03-11 19:56:49 +0300

    Updated to Conan2

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,8 @@
-from conans import ConanFile
+import os
+from conan import ConanFile
+from conan.tools.layout import basic_layout
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy
 
 
 class PackioConan(ConanFile):
@@ -7,6 +11,7 @@ class PackioConan(ConanFile):
     license = "MPL-2.0"
     author = "Quentin Chateau <quentin.chateau@gmail.com>"
     url = "https://github.com/qchateau/packio"
+    package_type = "header-library"
     description = "Asynchrnous msgpack-RPC and JSON-RPC server and client"
     topics = (
         "rpc",
@@ -21,53 +26,49 @@ class PackioConan(ConanFile):
     )
     exports_sources = "include/*"
     no_copy_source = True
-    options = {
-        "standalone_asio": [True, False],
-        "msgpack": [True, False],
-        "nlohmann_json": [True, False],
-        "boost_json": [True, False, "default"],
-    }
-    default_options = {
-        "standalone_asio": False,
-        "msgpack": True,
-        "nlohmann_json": True,
-        "boost_json": "default",  # defaults to True if using boost, False if using asio
-    }
+
+    @property
+    def _min_cppstd(self):
+        return "17"
+
+    def layout(self):
+        basic_layout(self)
+
+    def validate(self):
+        if self.settings.get_safe("compiler.cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+
+    def configure(self):
+        self.options["msgpack-cxx"].use_boost = False
 
     def requirements(self):
-        boost_require = None
-
-        if self.options.boost_json == "default":
-            self.options.boost_json = not self.options.standalone_asio
-
-        if self.options.msgpack:
-            self.requires("msgpack-cxx/4.1.3")
-        if self.options.nlohmann_json:
-            self.requires("nlohmann_json/3.9.1")
-        if self.options.boost_json:
-            boost_require = "boost/[>=1.75.0]"
-
-        if self.options.standalone_asio:
-            self.requires("asio/[>=1.13.0]")
-        elif not boost_require:
-            boost_require = "boost/[>=1.70.0]"
-
-        if boost_require:
-            self.requires(boost_require)
+        self.requires(
+            "msgpack-cxx/4.1.3", transitive_libs=True, transitive_headers=True
+        )
+        self.requires(
+            "nlohmann_json/3.11.3", transitive_libs=True, transitive_headers=True
+        )
+        self.requires("asio/[>=1.32.0]", transitive_libs=True, transitive_headers=True)
 
     def package(self):
-        self.copy("*.h")
+        copy(
+            self,
+            pattern="*.h",
+            dst=os.path.join(self.package_folder, "include"),
+            src=os.path.join(self.source_folder, "include"),
+        )
 
     def package_info(self):
-        self.cpp_info.defines.append(
-            f"PACKIO_STANDALONE_ASIO={1 if self.options.standalone_asio else 0}"
-        )
-        self.cpp_info.defines.append(
-            f"PACKIO_HAS_MSGPACK={1 if self.options.msgpack else 0}"
-        )
-        self.cpp_info.defines.append(
-            f"PACKIO_HAS_NLOHMANN_JSON={1 if self.options.nlohmann_json else 0}"
-        )
-        self.cpp_info.defines.append(
-            f"PACKIO_HAS_BOOST_JSON={1 if self.options.boost_json else 0}"
-        )
+        self.cpp_info.set_property("cmake_file_name", "packio")
+        self.cpp_info.set_property("cmake_target_name", "packio::packio")
+        self.cpp_info.requires = [
+            "msgpack-cxx::msgpack-cxx",
+            "asio::asio",
+            "nlohmann_json::nlohmann_json",
+        ]
+        self.cpp_info.defines.append("PACKIO_STANDALONE_ASIO=1")
+        self.cpp_info.defines.append("PACKIO_HAS_MSGPACK=1")
+        self.cpp_info.defines.append("PACKIO_HAS_NLOHMANN_JSON=1")
+        self.cpp_info.defines.append("PACKIO_HAS_BOOST_JSON=0")
+        self.cpp_info.libdirs = []
+        self.cpp_info.bindirs = []
--- test_package/certs/build_certs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-openssl req  -nodes -new -x509 -keyout server.key -out server.cert \
-    -subj "/C=FR/ST=None/L=/O=None/OU=/CN=/emailAddress="
